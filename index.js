2a5cd514ec4cbc59dbbc3e1924e8004bfbef7d4a
Thefutureseer <danielharrislimos@outlook.com>
2023-05-22 17:08:11 -0700

    add API KEY index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+const APIKEY = require('dotenv').config();
+
+async function generateText(prompt) {
+  try {
+    const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
+      prompt: prompt,
+      max_tokens: 100, // Adjust the number of tokens as per your requirement
+      temperature: 0.6, // Adjust the temperature to control the randomness of the generated text
+    }, {
+      headers: {
+        'Authorization': 'Bearer proccess.env.APIKEY',
+        'Content-Type': 'application/json',
+      },
+    });
+
+    return response.data.choices[0].text.trim();
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
+  }
+}
